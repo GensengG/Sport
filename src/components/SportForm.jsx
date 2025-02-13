7c545a888f61bfeeecbdbe89b918f127a2822161
GensengG <168531024+GensengG@users.noreply.github.com>
2025-02-13 22:38:27 +0300

    Update SportForm.jsx

--- src/components/SportForm.jsx
+++ src/components/SportForm.jsx
@@ -1,30 +1,63 @@
 import "../App.jsx";
 import { useState } from "react";
-import { SportRender } from "./SportRender.jsx";
 
 export const SportForm = () => {
-
     const [sportList, setSportList] = useState([]);
-    let sportListObj = [];
-    let sportListArr = [];
-    let result = [];
 
     let date = "";
     let distance = "";
+    let sportListArr = [];
+    let index = 0;
+    let newLine = {};
+
+    function test(newLine){
+        let testDate = newLine.date;
+        let count = 0;
+        let i = 0;
+        sportListArr.forEach(element => {
+            if(element.date === testDate){
+                count++;
+                i = element.id - 1;
+            } 
+        });
+
+        if(count > 0){
+            sportListArr[i].distance += newLine.distance;
+        } else {
+            sportListArr.push(newLine);
+        }
+        return sportListArr;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        date = e.target.date.value;
-        distance = e.target.distance.value;
-        
-        setSportList((sportList) => ({
-            ...sportList, 
-            [date]: distance,
-        }));
+        sportListArr = sportList;
+        index ++;
 
-        console.log(sportList);
+        date = Number(e.target.date.value);
+        distance = Number(e.target.distance.value);
+
+        newLine = {
+            id: index,
+            date: date,
+            distance: distance
+        }       
+
+        test (newLine);
+        sportList = sportListArr;
+        setSportList([
+            sportListArr
+        ]); 
     };
 
+    const handleDelete = (e) => {
+        e.preventDefault();
+        let element = e.target;
+        let elementDelete = element.parentElement;
+        let elementDeleteId = elementDelete.id - 1;
+        sportListArr.splice(elementDeleteId, 1);
+    }
+        
     return (
         <form className="sport__form" autoComplete="off" onSubmit={handleSubmit}>
             <div className="input__container">
@@ -38,7 +71,13 @@ export const SportForm = () => {
             <button type="submit" className="btn__submit">ОК</button>
 
             <div className="sport__list">
-                <SportRender initialObj = {sportList}/>
+                {sportList.map((item) => (
+                    <div key={item.id} id={item.id} className="list__line">
+                        <div className="list_date">{item.date}</div> 
+                        <div className="list__distance">{item.distance}</div> 
+                        <button className="list__edit" onClick={handleDelete}>✘</button>
+                    </div>
+                ))}
             </div>
         </form>
     );
